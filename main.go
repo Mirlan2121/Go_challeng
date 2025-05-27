1bb1a5cddd36e8316695221f74fd924f03a0caf7
Mirlan2121 <askarbekovmirlan40@gmail.com>
2025-05-27 14:23:56 +0900

    Функции: Aнонимны, Рекурсивные

--- main.go
+++ main.go
@@ -2,34 +2,61 @@ package main
 
 import "fmt"
 
-// Функции как параметры других функций
-func add(x int, y int) int {
-	return x + y
-}
-func subtract(x int, y int) int {
-	return x - y
-}
-func multiply(x int, y int) int {
-	return x * y
+// Анонимная функция как аргумент функции
+func action(n1 int, n2 int, operation func(int, int) int) {
+
+	result := operation(n1, n2)
+	fmt.Println(result)
 }
 
-// Функция как результат другой функции
+// Анонимная функция как результат функции
 func selectFn(n int) func(int, int) int {
 	if n == 1 {
-		return add
+		return func(x int, y int) int { return x + y }
 	} else if n == 2 {
-		return subtract
+		return func(x int, y int) int { return x - y }
 	} else {
-		return multiply
+		return func(x int, y int) int { return x * y }
 	}
 }
 
-// Функция как результат другой функции
+// Рекурсивные функции
+// Факиториал числа
+func factorial(n uint) uint {
+
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+//Фибоначи
+func fibbonachi(n uint) uint {
+
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	return fibbonachi(n-1) + fibbonachi(n-2)
+}
+
 func main() {
 
-	f := selectFn(1)
-	fmt.Println(f(3, 4)) // 7
+	action(10, 25, func(x int, y int) int { return x + y }) // 35
+	action(5, 6, func(x int, y int) int { return x * y })   // 30
 
-	f = selectFn(3)
-	fmt.Println(f(3, 4)) // 12
+	f := selectFn(1)
+	fmt.Println(f(2, 3)) // 5
+	fmt.Println(f(4, 5)) // 9
+	fmt.Println(f(7, 6)) // 13
+
+	fmt.Println(factorial(4)) // 24
+	fmt.Println(factorial(5)) // 120
+	fmt.Println(factorial(6)) // 720
+
+	fmt.Println(fibbonachi(4)) // 3
+	fmt.Println(fibbonachi(5)) // 5
+	fmt.Println(fibbonachi(6)) // 8
 }
