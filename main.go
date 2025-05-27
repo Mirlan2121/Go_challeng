e5f25519548c0a9decf4b39a7c054fafdfc83b5c
Mirlan2121 <askarbekovmirlan40@gmail.com>
2025-05-27 15:25:37 +0900

    Defer and Panic

--- main.go
+++ main.go
@@ -2,61 +2,16 @@ package main
 
 import "fmt"
 
-// Анонимная функция как аргумент функции
-func action(n1 int, n2 int, operation func(int, int) int) {
-
-	result := operation(n1, n2)
-	fmt.Println(result)
-}
-
-// Анонимная функция как результат функции
-func selectFn(n int) func(int, int) int {
-	if n == 1 {
-		return func(x int, y int) int { return x + y }
-	} else if n == 2 {
-		return func(x int, y int) int { return x - y }
-	} else {
-		return func(x int, y int) int { return x * y }
-	}
-}
-
-// Рекурсивные функции
-// Факиториал числа
-func factorial(n uint) uint {
-
-	if n == 0 {
-		return 1
-	}
-	return n * factorial(n-1)
-}
-
-//Фибоначи
-func fibbonachi(n uint) uint {
-
-	if n == 0 {
-		return 0
-	}
-	if n == 1 {
-		return 1
-	}
-	return fibbonachi(n-1) + fibbonachi(n-2)
-}
+// Defer
+//Оператор defer позволяет выполнить определенную функцию в
+//конце программы, при этом не важно, где в реальности вызывается эта функция.
 
 func main() {
+	defer finish()
+	fmt.Println("Program has been started")
+	fmt.Println("Program is working")
+}
 
-	action(10, 25, func(x int, y int) int { return x + y }) // 35
-	action(5, 6, func(x int, y int) int { return x * y })   // 30
-
-	f := selectFn(1)
-	fmt.Println(f(2, 3)) // 5
-	fmt.Println(f(4, 5)) // 9
-	fmt.Println(f(7, 6)) // 13
-
-	fmt.Println(factorial(4)) // 24
-	fmt.Println(factorial(5)) // 120
-	fmt.Println(factorial(6)) // 720
-
-	fmt.Println(fibbonachi(4)) // 3
-	fmt.Println(fibbonachi(5)) // 5
-	fmt.Println(fibbonachi(6)) // 8
+func finish() {
+	fmt.Println("Program has been finished")
 }
