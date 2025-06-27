5829aa8f07d4c5ee432e232fd6b3ad3f5bc37db1
Mirlan2121 <askarbekovmirlan40@gmail.com>
2025-06-27 16:13:19 +0900

    Мьютексты 2

--- main.go
+++ main.go
@@ -9,18 +9,26 @@ import (
 var counter int = 0 //  общий ресурс
 var wg sync.WaitGroup
 
-func work(number int) {
+func work(number int, mutex *sync.Mutex) {
+
+	mutex.Lock() // блокируем доступ к переменной counter
+
 	counter = 0 // сбрасываем общий ресурс
 
 	for k := 1; k <= 5; k++ {
-		counter += 1                       // изменяем общий ресурс
-		time.Sleep(100 * time.Millisecond) // задержка для наглядности
+		counter += 1 // изменяем общий ресурс
+		time.Sleep(100 * time.Millisecond)
 		fmt.Println("Goroutine", number, "-", counter)
 	}
+
+	mutex.Unlock() // деблокируем доступ
+
 	wg.Done() // сигнализируем, что горутина завершила работу
 }
 
 func main() {
+
+	var mutex sync.Mutex // определяем мьютекс
 
 	goroutines_count := 4 // количество запускаемых горутин
 
@@ -28,7 +36,7 @@ func main() {
 
 	// запускаем горутины
 	for i := 1; i <= goroutines_count; i++ {
-		go work(i)
+		go work(i, &mutex)
 	}
 	// ожидаем завершения всех горутин
 	wg.Wait()
