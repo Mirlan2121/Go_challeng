be5fc599a40d65a27b4935e1d87f817750af489e
Mirlan2121 <askarbekovmirlan40@gmail.com>
2025-06-28 11:41:23 +0900

    Тип error

--- main.go
+++ main.go
@@ -1,32 +1,22 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"os"
 )
 
-func factorial(n int) (int, error) {
+func open_file() {
 
-	if n < 0 {
-		return 0, errors.New("недопустимое число: должно быть положительным")
-	}
-
-	result := 1
-	for i := 1; i <= n; i += 1 {
-		result *= i
-	}
-	return result, nil // Возвращаем nil в качестве ошибки, если все хорошо
+	// открываем файл
+	file, err := os.Open("./main.go")
+	// закрываем файл в конце функции
+	defer file.Close()
+	// работа с файлом
+	fmt.Println("file:", file)
+	fmt.Println("error:", err)
 }
 
 func main() {
 
-	// Корректный параметр
-	fact, err := factorial(5)
-	fmt.Println("Factorial:", fact) // Factorial: 120
-	fmt.Println("Error:", err)      // Error: <nil>
-
-	// Некорректный параметр
-	fact, err = factorial(-5)
-	fmt.Println("Factorial:", fact) // Factorial: 0
-	fmt.Println("Error:", err)      // Error: недопустимое число: должно быть положительным
+	open_file()
 }
