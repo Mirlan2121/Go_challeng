8dfd8a09f747c15480627e88313c0d08bf169c38
Mirlan2121 <askarbekovmirlan40@gmail.com>
2025-07-01 14:08:41 +0900

    Восстановление после ошибки и функция recover

--- main.go
+++ main.go
@@ -2,18 +2,15 @@ package main
 
 import "fmt"
 
-func divide_defer() {
+func try_catch() {
 
-	fmt.Println("divide_defer executed")
-}
-
-func main_defer() {
-
-	fmt.Println("main_defer executed")
+	if r := recover(); r != nil {
+		fmt.Println("Error:", r)
+	}
 }
 
 func divide(x, y float64) float64 {
-	defer divide_defer()
+	defer try_catch()
 	if y == 0 {
 		panic("Division by zero!")
 	}
@@ -21,32 +18,6 @@ func divide(x, y float64) float64 {
 }
 
 func main() {
-	defer main_defer()
 	fmt.Println(divide(4, 0))
 	fmt.Println("Program has been finished")
 }
-
-//Оператор defer помещает вызов переданной ему функции в стек. Когда окружающая функция возвращает управление, вызываются все
-//функции, хранящиеся в стеке. Функция, хранящаяся в конце стека, выполняется первой. Значения, переданные в аргументах функции
-//defer, вычисляются, когда компилятор находит оператор defer.
-
-//В функциях defer могут выполняться как операции чтения, так и записи.
-
-//Возвращаемые значения могут быть изменены в функциях defer.
-
-//Оператор defer обеспечивает выполнение отложенных функций даже в случае ошибки или вызова функции panic, помогая очищать
-//ресурсы.
-
-// Пример
-//package main
-//import "fmt"
-//
-//func main() {
-//    defer finish()
-//    fmt.Println("Program has been started")
-//    fmt.Println("Program is working")
-//}
-//
-//func finish(){
-//    fmt.Println("Program has been finished")
-//}
