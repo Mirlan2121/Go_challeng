0abf4140f8b6a8428a05997574694d91e6cf1b82
Mirlan2121 <askarbekovmirlan40@gmail.com>
2025-07-01 14:38:46 +0900

    Создание и открытие файлов

--- main.go
+++ main.go
@@ -1,27 +1,16 @@
 package main
 
-import "fmt"
-
-type phoneWriter struct{}
-
-func (p phoneWriter) Write(bs []byte) (int, error) {
-	if len(bs) == 0 {
-		return 0, nil
-	}
-	for i := 0; i < len(bs); i++ {
-		if bs[i] >= '0' && bs[i] <= '9' {
-			fmt.Print(string(bs[i]))
-		}
-	}
-	fmt.Println()
-	return len(bs), nil
-}
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	bytes1 := []byte("+1(234)567 9010")
-	bytes2 := []byte("+2-345-678-12-35")
-
-	writer := phoneWriter{}
-	writer.Write(bytes1)
-	writer.Write(bytes2)
+	file, err := os.Create("hello.txt") // создаем файл
+	if err != nil {                     // если возникла ошибка
+		fmt.Println("Unable to create file:", err)
+		os.Exit(1) // выходим из программы
+	}
+	defer file.Close()       // закрываем файл
+	fmt.Println(file.Name()) // hello.txt
 }
