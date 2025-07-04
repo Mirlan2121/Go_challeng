1ef88c55db52342d2df9f48458d907024d97776c
Mirlan2121 <askarbekovmirlan40@gmail.com>
2025-07-04 15:57:40 +0900

    http.Client

--- main.go
+++ main.go
@@ -8,7 +8,15 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("https://google.com")
+	client := &http.Client{}
+	req, err := http.NewRequest(
+		"GET", "https://google.com", nil,
+	)
+	// добавляем заголовки
+	req.Header.Add("Accept", "text/html")     // добавляем заголовок Accept
+	req.Header.Add("User-Agent", "MSIE/15.0") // добавляем заголовок User-Agent
+
+	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println(err)
 		return
