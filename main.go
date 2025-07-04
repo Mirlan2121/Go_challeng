d6a3e09372e97572495d7d4bcf3f6e928daaab2a
Mirlan2121 <askarbekovmirlan40@gmail.com>
2025-07-04 15:47:28 +0900

    Отправка запросов по HTTP

--- main.go
+++ main.go
@@ -2,57 +2,17 @@ package main
 
 import (
 	"fmt"
-	"net"
+	"io"
+	"net/http"
+	"os"
 )
 
-var dict = map[string]string{
-	"red":    "красный",
-	"green":  "зеленый",
-	"blue":   "синий",
-	"yellow": "желтый",
-	"Miki":   "Мики",
-}
-
 func main() {
-	listener, err := net.Listen("tcp", ":4545")
-
+	resp, err := http.Get("https://google.com")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer listener.Close()
-	fmt.Println("Server is listening...")
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println(err)
-			conn.Close()
-			continue
-		}
-		go handleConnection(conn) // запускаем горутину для обработки запроса
-	}
-}
-
-// обработка подключения
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	for {
-		// считываем полученные в запросе данные
-		input := make([]byte, (1024 * 4))
-		n, err := conn.Read(input)
-		if n == 0 || err != nil {
-			fmt.Println("Read error:", err)
-			break
-		}
-		source := string(input[0:n])
-		// на основании полученных данных получаем из словаря перевод
-		target, ok := dict[source]
-		if ok == false { // если данные не найдены в словаре
-			target = "undefined"
-		}
-		// выводим на консоль сервера диагностическую информацию
-		fmt.Println(source, "-", target)
-		// отправляем данные клиенту
-		conn.Write([]byte(target))
-	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
 }
